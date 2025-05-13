da430c66a090e67737899e334c226c49938e6d04
Laassel <lassel.giovanni@gmail.com>
2025-05-13 16:00:50 +0200

    Snack 1 e Snack 2: somma e quadrato con arrow functions

---
+++ snacks/snack1.js
@@ -0,0 +1,14 @@
+// Snack 1
+
+// function somma(num1, num2) {
+//     return num1 + num2
+// }
+
+// const somma = function(num1, num2){
+//     return num1 + num2
+// }
+
+// console.log(somma(4, 6));
+
+const somma = (num1, num2) => (num1 + num2);
+console.log(somma(5, 7))
---
+++ snacks/snack2.js
@@ -0,0 +1,4 @@
+// Snack 2
+
+const quadrato = x => x * x;
+console.log(quadrato(50))
